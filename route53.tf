8254ae1ff17d0a95fc422e44c8c4ba2e211282ba
hamza-nb <hamzanb8@gmail.com>
2024-04-22 12:59:21 +0100

    add certif record to route53

---
+++ route53.tf
@@ -0,0 +1,24 @@
+data "aws_route53_zone" "route53_zone_info" {
+  name         = "${var.domain_name}."
+#   private_zone = true
+}
+
+resource "aws_route53_record" "add_certif_record_to_route53" {
+  depends_on = [ aws_acm_certificate.cert ]
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.route53_zone_info.zone_id
+}
+
+
