8449b5e41e39239fa14a8fba6944f1e2b6e59102
hamza-nb <hamzanb8@gmail.com>
2024-04-22 12:44:30 +0100

    ATTACH the HTTPS listener to the LB

--- load_balancer_listeners.tf
+++ load_balancer_listeners.tf
@@ -14,3 +14,21 @@ depends_on = [ aws_lb.lb, aws_lb_target_group.terraform_target_group ]
   }
 }
 
+resource "aws_lb_listener" "https_lb_listener" {
+depends_on = [ aws_lb.lb, aws_lb_target_group.terraform_target_group, aws_route53_record.add_certif_record_to_route53 ]
+
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.cert.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.terraform_target_group.arn
+  }
+  tags = {
+    "Name" = "${var.app_name}"
+  }
+}
+
