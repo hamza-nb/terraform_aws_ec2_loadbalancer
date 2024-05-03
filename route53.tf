d165431152536c3a48698a3f8fe83154b7ad3e40
hamza-nb <hamzanb8@gmail.com>
2024-05-03 11:23:17 +0100

    Add a record in route53 to link certif with LB

--- route53.tf
+++ route53.tf
@@ -21,4 +21,15 @@ resource "aws_route53_record" "add_certif_record_to_route53" {
   zone_id         = data.aws_route53_zone.route53_zone_info.zone_id
 }
 
+resource "aws_route53_record" "route53_lb" {
+  depends_on = [ aws_lb.lb ]
+  zone_id = data.aws_route53_zone.route53_zone_info.zone_id
+  name    = "${var.app_name}.${var.domain_name}"
+  type    = "A"
 
+  alias {
+    name                   = aws_lb.lb.dns_name
+    zone_id                = aws_lb.lb.zone_id
+    evaluate_target_health = true
+  }
+}
